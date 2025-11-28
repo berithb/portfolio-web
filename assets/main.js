e4160eb38960de7fd96da8e9aaa64f7f9a164b1a
Belite <batybelite12@gmail.com>
2025-11-28 16:16:19 +0200

    added new feature

---
+++ assets/main.js
@@ -0,0 +1,8 @@
+document.addEventListener( "DOMContentLoaded", function(){
+    const mainNav = document.querySelector('.main-nav');
+    const profileCard = document.querySelector('.profile-card');
+
+    const menuToggleBtn = document.getElementById('menu-toggle');
+
+
+})
